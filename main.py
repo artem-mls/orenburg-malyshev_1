ff059fb89325b349e8954cda94bbb16bfa655853
Малышев <malyshev.ao@oksei>
2022-11-21 17:39:33 +0500

    Рисуются жёлтые круги

---
+++ main.py
@@ -0,0 +1,39 @@
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QPainter, QColor
+from random import randint
+import UI
+
+
+class YellowCircles(QWidget, UI.Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.paint = False
+
+        self.pushButton.clicked.connect(self.button_click)
+
+    def paintEvent(self, event):
+        if self.paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+
+    def draw(self, qp):
+        qp.setBrush(QColor('yellow'))
+        count = randint(3, 10)
+        for i in range(count):
+            x, y = randint(0, self.size().width()), randint(0, self.size().height())
+            w = h = randint(5, 200)
+            qp.drawEllipse(x, y, w, h)
+
+    def button_click(self):
+        self.paint = True
+        self.repaint()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    wnd = YellowCircles()
+    wnd.show()
+    app.exec_()
